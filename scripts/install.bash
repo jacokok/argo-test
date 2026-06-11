35ba48b4e2e4ed097a29aabb80c1e402aad724a6
Jaco Kok <kokjaco2@gmail.com>
2026-06-11 15:08:51 +0200

    Latest

--- scripts/install.bash
+++ scripts/install.bash
@@ -6,7 +6,6 @@ k3s kubectl get nodes
 
 # ssh doink@k3s "sudo cat /etc/rancher/k3s/k3s.yaml" | sed -e 's/127.0.0.1/k3s/g' -e 's/localhost/k3s/g' > ~/.kube/config && chmod 600 ~/.kube/config && kubectl cluster-info
 scp doink@k3s:/etc/rancher/k3s/k3s.yaml ~/.kube/config
-sed -i '' -e 's/127.0.0.1/k3s/g' -e 's/localhost/k3s/g' ~/.kube/config
 sed -i 's/127.0.0.1/k3s/g' ~/.kube/config && sed -i 's/localhost/k3s/g' ~/.kube/config
 chmod 600 ~/.kube/config
 kubectl cluster-info
